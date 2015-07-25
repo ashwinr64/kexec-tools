ae23f987745049009ee97d4259d904648518180f
Ashwin Ramesh <ashwinr64@gmail.com>
2015-07-25 17:32:52 +0530

    Some updates

--- kexec/arch/arm/mach-victara.c
+++ kexec/arch/arm/mach-victara.c
@@ -3,7 +3,6 @@
 #include <libfdt.h>
 
 #include "../../kexec.h"
-#include "../../fs2dt.h"
 #include "mach.h"
 
 #define INVALID_SOC_REV_ID 0xFFFFFFFF
@@ -13,6 +12,7 @@ struct msm_id
     uint32_t platform_id;
     uint32_t hardware_id;
     uint32_t soc_rev;
+    uint32_t board_rev;
 };
 
 static uint32_t victara_dtb_compatible(void *dtb, struct msm_id *devid, struct msm_id *dtb_id)
@@ -32,6 +32,8 @@ static uint32_t victara_dtb_compatible(void *dtb, struct msm_id *devid, struct m
     if (!prop || len <= 0) {
         printf("DTB: qcom,msm-id entry not found\n");
         return 0;
+    } else if(len == 12) {
+        dtb_id->board_rev = 0;
     } else if (len < (int)sizeof(struct msm_id)) {
         printf("DTB: qcom,msm-id entry size mismatch (%d != %d)\n",
             len, sizeof(struct msm_id));
@@ -41,9 +43,11 @@ static uint32_t victara_dtb_compatible(void *dtb, struct msm_id *devid, struct m
     dtb_id->platform_id = fdt32_to_cpu(((const struct msm_id *)prop)->platform_id);
     dtb_id->hardware_id = fdt32_to_cpu(((const struct msm_id *)prop)->hardware_id);
     dtb_id->soc_rev = fdt32_to_cpu(((const struct msm_id *)prop)->soc_rev);
+    if(len > 12)
+        dtb_id->board_rev = fdt32_to_cpu(((const struct msm_id *)prop)->board_rev);
 
-    //printf("DTB: found dtb platform %u hw %u soc 0x%x\n",
-    //      dtb_id->platform_id, dtb_id->hardware_id, dtb_id->soc_rev);
+    //printf("DTB: found dtb platform %u hw %u soc 0x%x board %u\n",
+    //      dtb_id->platform_id, dtb_id->hardware_id, dtb_id->soc_rev, dtb_id->board_rev);
 
     if (dtb_id->platform_id != devid->platform_id ||
         dtb_id->hardware_id != devid->hardware_id) {
@@ -60,8 +64,10 @@ static int victara_choose_dtb(const char *dtb_img, off_t dtb_len, char **dtb_buf
     FILE *f;
     struct msm_id devid, dtb_id;
     char *bestmatch_tag = NULL;
+    size_t id_read = 0;
     uint32_t bestmatch_tag_size;
     uint32_t bestmatch_soc_rev_id = INVALID_SOC_REV_ID;
+    uint32_t bestmatch_board_rev_id = INVALID_SOC_REV_ID;
 
     f = fopen("/proc/device-tree/qcom,msm-id", "r");
     if(!f)
@@ -70,15 +76,19 @@ static int victara_choose_dtb(const char *dtb_img, off_t dtb_len, char **dtb_buf
         return 0;
     }
 
-    fread(&devid, sizeof(struct msm_id), 1, f);
+    id_read = fread(&devid, 1, sizeof(struct msm_id), f);
     fclose(f);
 
     devid.platform_id = fdt32_to_cpu(devid.platform_id);
     devid.hardware_id = fdt32_to_cpu(devid.hardware_id);
     devid.soc_rev = fdt32_to_cpu(devid.soc_rev);
+    if(id_read > 12)
+        devid.board_rev = fdt32_to_cpu(devid.board_rev);
+    else
+        devid.board_rev = 0;
 
-    printf("DTB: platform %u hw %u soc 0x%x\n",
-            devid.platform_id, devid.hardware_id, devid.soc_rev);
+    printf("DTB: platform %u hw %u soc 0x%x board %u\n",
+            devid.platform_id, devid.hardware_id, devid.soc_rev, devid.board_rev);
 
     while(dtb + sizeof(struct fdt_header) < dtb_end)
     {
@@ -99,24 +109,29 @@ static int victara_choose_dtb(const char *dtb_img, off_t dtb_len, char **dtb_buf
 
         if(victara_dtb_compatible(dtb, &devid, &dtb_id))
         {
-            if (dtb_id.soc_rev == devid.soc_rev)
+            if (dtb_id.soc_rev == devid.soc_rev &&
+                dtb_id.board_rev == devid.board_rev)
             {
                 *dtb_buf = xmalloc(dtb_size);
                 memcpy(*dtb_buf, dtb, dtb_size);
                 *dtb_length = dtb_size;
-                printf("DTB: match 0x%x, my id 0x%x , len %u\n",
-                        dtb_id.soc_rev, devid.soc_rev, dtb_size);
+                printf("DTB: match 0x%x %u, my id 0x%x %u, len %u\n",
+                        dtb_id.soc_rev, dtb_id.board_rev,
+                        devid.soc_rev, devid.board_rev, dtb_size);
                 return 1;
             }
-            else if(dtb_id.soc_rev <= devid.soc_rev)
+            else if(dtb_id.soc_rev <= devid.soc_rev &&
+                    dtb_id.board_rev < devid.board_rev)
             {
                 if((bestmatch_soc_rev_id == INVALID_SOC_REV_ID) ||
                     (bestmatch_soc_rev_id < dtb_id.soc_rev) ||
-                    (bestmatch_soc_rev_id == dtb_id.soc_rev))
+                    (bestmatch_soc_rev_id == dtb_id.soc_rev &&
+                    bestmatch_board_rev_id < dtb_id.board_rev))
                 {
                     bestmatch_tag = dtb;
                     bestmatch_tag_size = dtb_size;
                     bestmatch_soc_rev_id = dtb_id.soc_rev;
+                    bestmatch_board_rev_id = dtb_id.board_rev;
                 }
             }
         }
@@ -130,8 +145,9 @@ static int victara_choose_dtb(const char *dtb_img, off_t dtb_len, char **dtb_buf
     }
 
     if(bestmatch_tag) {
-        printf("DTB: bestmatch 0x%x, my id 0x%x\n",
-                bestmatch_soc_rev_id, devid.soc_rev);
+        printf("DTB: bestmatch 0x%x %u, my id 0x%x %u\n",
+                bestmatch_soc_rev_id, bestmatch_board_rev_id,
+                devid.soc_rev, devid.board_rev);
         *dtb_buf = xmalloc(bestmatch_tag_size);
         memcpy(*dtb_buf, bestmatch_tag, bestmatch_tag_size);
         *dtb_length = bestmatch_tag_size;
@@ -172,7 +188,7 @@ static int victara_add_extra_regs(void *dtb_buf)
 }
 
 const struct arm_mach arm_mach_victara = {
-    .boardnames = { "victara", NULL },
+    .boardnames = { "victara", "bacon", "d851", "d855", NULL },
     .choose_dtb = victara_choose_dtb,
     .add_extra_regs = victara_add_extra_regs,
 };
